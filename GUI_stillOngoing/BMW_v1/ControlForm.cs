4006a61205480aeccbb8af9d998cfb4abc73651a
tjao <tjao@umass.edu>
2015-01-20 12:46:54 +0800

    Draft of GUI (1)
    not yet adding database
    Only layout of what training may look like

---
+++ GUI_stillOngoing/BMW_v1/ControlForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BMW_v1
+{
+    public partial class ControlForm : Form
+    {
+        public ControlForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
